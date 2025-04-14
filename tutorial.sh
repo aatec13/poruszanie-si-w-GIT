9f12278593ca96bc1367c61990a6d93d9ffc9454
aatec13 <saganzuzia@gmail.com>
2025-04-14 21:26:58 +0200

    Aktualizacja

--- tutorial.sh
+++ tutorial.sh
@@ -1,20 +1,26 @@
 git commit
 git branch myBranch
-git checkout/switch <nazwa-galęzi> ; galąź z którą chcemy pracować
-git merge ; metoda scalania (lączenia) galęzi
-git chceckout bugFix; git merge main - laczenie galezi main z galezia bugfix
+git checkout/switch <nazwa-galęzi> 
+git merge 
 -f 
-git rebase <nazwa> - przeniesć prace z galezni na inną galąź
-^ , czyli git checkout <nazwa-galęzi>^ - poruszanie się wstecz o jednen commit
-~<num> - poruszanie się wstecz o ileś commitów
-git reset <nazwa>~<num> - odwraca zmiany poprzez przesunięcie referencji gałęzi wstecz w czasie do starszego commita.
-git revert HEAD - w celu odwrócenia zmian i podzielenia się nimi z innymi
-git cherry-pick <Commit1> <Commit2> <...> - Jest to bardzo prosty sposób określenia, że chcesz przenieść zmiany poniżej swojej obecnej lokalizacji (HEAD)
-git rebase -i <nazwa> - Jeśli włączysz tę opcję, Git otworzy okno, aby pokazać ci, które commity mają być skopiowane poniżej wskazanego celu przebazowania (np. HEADa). W oknie pokażą się również wiadomości i hasze commitów, żeby ułatwić ci zrozumienie, co jest czym.
-Możesz zdecydować się zachować wszystkie commity lub pominąć niektóre z nich. Jest to oznaczone przez aktywny przycisk pick. Wyłączenie przycisku spowoduje pominięciu commitu.
+git rebase <nazwa> 
+^ 
+~<num> 
+git reset <nazwa>~<num> 
+git revert <nazwa>
+git cherry-pick <Commit1> <Commit2> <...> 
+git rebase -i <nazwa> 
 git commit --amend
 git tag <nazwa tagu> <nazwa>
-git describe <ref> - może pomóc ci zorientować się w sytuacji, gdy przesuniesz wiele commitów wstecz lub do przodu w historii
+git describe <ref> 
 wygląd polecenia : <tag>_<numCommits>_g<hash
-
+git clone
+/o
+git fetch
+git pull
+git fakeTeamwork
+git push
+git pull --rebase
+foo
+git push origin <nazwa>
 
